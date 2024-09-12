c451e3cbc3f625dd7e0f4ae85bab78808cb296c6
Cesar Ferracin <cesarferracin@gmail.com>
2024-09-12 11:34:01 +0100

    in.Orbit React app code

---
+++ src/app.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getSummary } from './http/get-summary'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+
+
+export function App() {
+
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60 // 60 seconds
+  })
+
+  return (
+    <Dialog>
+      {data && data.total > 0 ? <Summary /> : <EmptyGoals />}
+      <CreateGoal />
+    </Dialog>
+  )
+}
